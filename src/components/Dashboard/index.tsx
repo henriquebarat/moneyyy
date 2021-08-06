b6ee8e043ee972018d6098bcb4b7bb77998a416d
henriquebarat <henriquelbarat@gmail.com>
2021-08-06 00:27:59 -0300

    second commit

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Summarry } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable";
+import { Container } from "./styles";
+
+export function Dashboard(){
+    return(
+        <Container>
+            <Summarry />
+            <TransactionsTable />
+        </Container>    
+    )
+} 
